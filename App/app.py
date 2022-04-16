98dcbc2b03b34f88e1d4285e0586e12d99eb7325
m20210672 <90702822+m20210672@users.noreply.github.com>
2022-04-16 17:33:31 +0100

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,5 +1,6 @@
 
 import dash
+import tkinter
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
@@ -12,6 +13,7 @@ import pandas as pd
 from skimage import io
 import plotly.graph_objs as go
 import plotly.express as px
+
 
 
 
